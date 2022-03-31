d0d7f31a11d5d4a2823cb57e22a27a7e9e350b9b
JustAo <51353623+Love2Bit@users.noreply.github.com>
2022-03-31 14:31:20 +0700

    update footer
    update footer

--- inc/footer.php
+++ inc/footer.php
@@ -3,7 +3,7 @@
 	<footer class="footer">
     <div class="col-container">
       <svg viewbox="0 0 64 64" class="logo-icon"><use xlink:href="#logo_icon"></use></svg>
-  		<p class="footer-copy">&copy; <?php echo date("Y"); ?> Time Tracker by Muhammad Rimba</p>
+  		<p class="footer-copy">&copy; <?php echo date("Y"); ?> Time Tracker by Aodellas Magosiang</p>
     </div>
 	</footer>
 
